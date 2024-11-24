8e4827795215fc09b96e879a21798f4c598ee818
Kritica Agarwal <kriticaagarwal@gmail.com>
2024-11-24 15:54:27 +0530

    Many to Many relationship

--- JPA/src/main/java/com/kritica/jpa/models/SocialUsers.java
+++ JPA/src/main/java/com/kritica/jpa/models/SocialUsers.java
@@ -3,7 +3,9 @@ package com.kritica.jpa.models;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 @Entity
@@ -24,4 +26,10 @@ public class SocialUsers {
     @OneToMany(mappedBy = "socialUser")
     private List<Post> posts= new ArrayList<Post>();
 
+    @ManyToMany
+    @JoinTable(
+        joinColumns = @JoinColumn(name="user_id"),
+        inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    private Set<SocialGroup> groups= new HashSet<SocialGroup>();
 }
